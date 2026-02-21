bdf2a6ba67011f55bc21d9e398b3fe594221a354
Jose Miguel <josemi.arandafernandez@gmail.com>
2026-02-21 12:52:41 +0100

    Crear página de registro

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,29 @@
+<form action="{{ route('auth.store') }}" method="post">
+    @csrf
+    <label for="name">Nombre:</label>
+    <input type="text" id="name" name="name" placeholder="Jhon Doe" value="{{ old('name') }}">
+    @error('name')
+        <p>{{ $message }}</p>
+    @enderror
+    <br>
+
+    <label for="email">Correo electrónico: </label>
+    <input type="email" id="email" name="email" placeholder="jhon@email.com" value="{{ old('email') }}">
+    @error('email')
+        <p>{{ $message }}</p>
+    @enderror
+    <br>
+
+    <label for="password">Contraseña</label>
+    <input type="password" id="password" name="password">
+    @error('password')
+        <p>{{ $message }}</p>
+    @enderror
+    <br>
+
+    <label for="password_confirmation">Confirmar Contraseña</label>
+    <input type="password" id="password_confirmation" name="password_confirmation">
+    <br>
+
+    <button>Registrarse</button>
+</form>

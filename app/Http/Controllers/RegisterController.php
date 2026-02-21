2a4a0d92f31119034071464e74ebf458e2a40aec
Jose Miguel <josemi.arandafernandez@gmail.com>
2026-02-21 12:47:05 +0100

    Corregir retornar view

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,7 +10,7 @@ use Illuminate\Validation\Rules\Password;
 class RegisterController extends Controller
 {
     public function create() {
-        return ('auth.register');
+        return view('auth.register');
     }
 
     public function register(Request $request) {
